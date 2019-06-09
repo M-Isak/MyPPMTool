ff76b53eb28943d7e6c2431e17284954971f88c3
Mohamed Ali <mohamed.ali@luulsolutions.com>
2019-06-09 17:51:58 +0100

    Delete project by Identifier

--- ppmtool/src/main/java/com/mohaisak/ppmtool/services/ProjectService.java
+++ ppmtool/src/main/java/com/mohaisak/ppmtool/services/ProjectService.java
@@ -47,5 +47,16 @@ public class ProjectService {
 		return projectRepository.findAll();
 	}
 	
+	public void deleteProjectByIdentifier(String projectId) {
+		
+	Project project = projectRepository.findByProjectIdentifier(projectId.toUpperCase());
+		
+		if (project == null) {
+			  throw new ProjectIdException("cannot delete project '"+projectId.toUpperCase()+"'. it deos not Exist");
+		}
+		
+		projectRepository.delete(project);
+	}
+	
 	
 }
