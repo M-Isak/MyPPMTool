6f7e0b4789c0a09afacb9bd6c0220d238d031785
Mohamed Ali <mohamed.ali@luulsolutions.com>
2019-06-14 11:35:46 +0100

    react first component aadded

--- ppmtool-react-client/src/App.js
+++ ppmtool-react-client/src/App.js
@@ -1,29 +1,14 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Dashboard from "./components/Dashboard";
 
 function App() {
-    return ( <
-        div className = "App" >
+    return ( < div className = "App" >
+
         <
-        header className = "App-header" >
+        Dashboard / >
+
         <
-        img src = {
-            logo
-        }
-        className = "App-logo"
-        alt = "logo" / >
-        <
-        p >
-        Now < code > Welcome < /code> to PPMTOOL! < /
-        p > <
-        a className = "App-link"
-        href = "https://reactjs.org"
-        target = "_blank"
-        rel = "noopener noreferrer" >
-        Learn React <
-        /a> < /
-        header > <
         /div>
     );
 }
